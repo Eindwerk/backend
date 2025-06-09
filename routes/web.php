79630db63e9dbcf5bef178dc341af47092312f2d
Cédric <cedricvanhoorebeke@outlook.com>
2025-06-09 17:56:33 +0200

    Add logout route with authentication and throttling middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminRegisterController;
+use Filament\Http\Controllers\Auth\LogoutController;
 
 // GET shows the password form, with signed validation
 Route::get('/register/{user}', [AdminRegisterController::class, 'showForm'])
@@ -11,3 +12,7 @@ Route::get('/register/{user}', [AdminRegisterController::class, 'showForm'])
 // POST saves the password
 Route::post('/register/{user}', [AdminRegisterController::class, 'storePassword'])
     ->name('admin.register.submit');
+
+Route::post('/logout', [LogoutController::class, 'logout'])
+    ->name('admin.logout')
+    ->middleware(['auth:filament', 'throttle:60,1']);
