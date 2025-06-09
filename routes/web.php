5f20a630e012dc3ccc0f01e914fd815f6fc6f1da
Cédric <cedricvanhoorebeke@outlook.com>
2025-06-09 18:06:57 +0200

    Remove logout route and associated middleware from web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminRegisterController;
-use Filament\Http\Controllers\Auth\LogoutController;
 
 // GET shows the password form, with signed validation
 Route::get('/register/{user}', [AdminRegisterController::class, 'showForm'])
@@ -12,7 +11,3 @@ Route::get('/register/{user}', [AdminRegisterController::class, 'showForm'])
 // POST saves the password
 Route::post('/register/{user}', [AdminRegisterController::class, 'storePassword'])
     ->name('admin.register.submit');
-
-Route::post('/logout', [LogoutController::class, 'logout'])
-    ->name('admin.logout')
-    ->middleware(['auth:filament', 'throttle:60,1']);
