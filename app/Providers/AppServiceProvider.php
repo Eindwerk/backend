14725de361d4b11fbd88ef1720827a77677be3c6
Cédric <cedricvanhoorebeke@outlook.com>
2025-06-11 22:10:15 +0200

    Herstel naar commit 6c87801

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if (env('APP_ENV') !== 'local') {
-            $url->forceScheme('https');
+        if (app()->environment('production')) {
         }
     }
 }
